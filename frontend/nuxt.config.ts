e352f0a0beda4b55b4ba345e65c44531fef082ac
SugitaMotoki <ce235019@meiji.ac.jp>
2023-10-31 11:34:03 +0900

    formatの修正

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,10 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/eslint-module',
-  ],
+  modules: ["@nuxtjs/eslint-module"],
   eslint: {
     cache: false,
   },
-  devtools: { enabled: true }
+  devtools: { enabled: true },
 });
