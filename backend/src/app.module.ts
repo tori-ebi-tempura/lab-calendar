dcb5e14fc7cdb982cd42eb90d78d9c687fe3f502
Reiya Matsumoto <ce235037@meiji.ac.jp>
2023-11-01 22:54:38 +0900

    add: classesのひな型

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClassesModule } from './classes/classes.module';
 
 @Module({
-  imports: [],
+  imports: [ClassesModule],
   controllers: [AppController],
   providers: [AppService],
 })
