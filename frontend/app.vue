225468241d9b88b9a25b35394128e3040dec6743
SugitaMotoki <ce235019@meiji.ac.jp>
2023-11-01 13:28:54 +0900

    vueファイルをレイアウトとページに分割

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <v-app id="lab-calendar">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </v-app>
 </template>
+
+<script setup lang="ts">
+useHead({
+  meta: [
+    {
+      charset: "utf-8",
+    },
+  ],
+  title: "Lab-Calendar",
+});
+</script>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <h1>Hello</h1>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: "トップページ - Lab-Calendar",
+});
+</script>
