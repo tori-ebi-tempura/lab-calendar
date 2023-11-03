255d2e572403c1901ef48bedd0fe0f09af8701f3
SugitaMotoki <ce235019@meiji.ac.jp>
2023-11-03 15:00:46 +0900

    プロパティを1行ずつ書く記法に変更

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
-  <v-app-bar :elevation="10" color="cyan-darken-1" height="80">
+  <v-app-bar
+    :elevation="10"
+    color="cyan-darken-1"
+    height="80"
+  >
     <v-container class="fill-height d-flex align-center">
       <v-app-bar-title>
-        <v-btn variant="text" to="/" size="x-large" :active="false" nuxt>
+        <v-btn
+          variant="text"
+          to="/"
+          size="x-large"
+          :active="false"
+          nuxt
+        >
           <span class="text-h5"> Lab-Calendar </span>
         </v-btn>
       </v-app-bar-title>
