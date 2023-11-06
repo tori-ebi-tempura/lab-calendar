c5fc4c28e6220ecc6cd6715ed07611229e5cf37d
Reiya Matsumoto <ce235037@meiji.ac.jp>
2023-11-06 22:22:19 +0900

    update: useGuradからマジックナンバーを削除

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Post, Request, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
+import { LocalAuthGuard } from "./local-auth.guard";
 
 @Controller("auth")
 export class AuthController {
-  @UseGuards(AuthGuard("local"))
+  @UseGuards(LocalAuthGuard)
   @Post("/login")
   async login(@Request() req) {
     return req.user;
