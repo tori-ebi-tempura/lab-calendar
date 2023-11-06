4cf5429cf4a9bcf7c80b646f08cbeb9bce4768fa
Reiya Matsumoto <ce235037@meiji.ac.jp>
2023-11-07 01:35:13 +0900

    add: API設計の追加

--- backend/src/klasses/dto/read-klass.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateKlassDto } from "./create-klass.dto";
-
-export class ReadKlassDto extends PartialType(CreateKlassDto) {
-  // link: {
-  //   rel: "self";
-  //   href: string;
-  // };
-}
