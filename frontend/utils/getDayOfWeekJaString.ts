b1b48a4bed50c83a99ed9aab362cae5d23c26bef
SugitaMotoki <ce235019@meiji.ac.jp>
2023-11-10 15:51:07 +0900

    数字で指定された曜日を返す関数の作成

---
+++ frontend/utils/getDayOfWeekJaString.ts
@@ -0,0 +1,5 @@
+/* eslint-disable func-names */
+export default function (dayOfWeekNumber: number) {
+  const dayOfWeekJaStrings = ["日", "月", "火", "水", "木", "金", "土"];
+  return dayOfWeekJaStrings[dayOfWeekNumber];
+}
