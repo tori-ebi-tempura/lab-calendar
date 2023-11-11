0aeccd22e3e8deccd38e781bece103120369622a
SugitaMotoki <ce235019@meiji.ac.jp>
2023-11-11 14:59:34 +0900

    Klassコンポーネントの追加

---
+++ frontend/components/calendar/week/Klass.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-btn
+    size="small"
+    style="position: absolute; z-index: 2"
+    :style="styleObject"
+  >
+    Foo
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+import type { Klass } from "~/types/klass";
+interface Props {
+  klass: Klass;
+}
+const { klass } = defineProps<Props>();
+
+const styleObject = reactive({
+  visibility: "visible",
+  top: "0px",
+  height: "0px",
+});
+const setStyle = (_klass: Klass) => {
+  const { fromHour, fromMinute, rangeMinute } = parseTimeString(
+    _klass.from,
+    _klass.to,
+  );
+  const timeDividerId = `time-divider-${fromHour}`;
+  const borderElement = document.getElementById(timeDividerId);
+  if (!borderElement) {
+    return;
+  }
+
+  styleObject.top = `calc(${borderElement.getBoundingClientRect().bottom}px + ${
+    fromMinute / 10
+  }vh)`;
+  styleObject.height = `${rangeMinute / 10}vh`;
+};
+
+onMounted(() => {
+  setStyle(klass);
+});
+
+window.addEventListener("resize", () => {
+  setStyle(klass);
+});
+</script>
